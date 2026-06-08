ba1d46da8251f7979465104693cbbce12964125b
Dhruv Talati <dhruvtalati640@gmail.com>
2026-06-09 01:43:36 +0530

    Fix browser preflight CORS

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -4,14 +4,16 @@ const cookieParser = require("cookie-parser");
 
 const app = express();
 
-app.use(express.json());
-app.use(cookieParser());
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://localhost:3000"],
+    origin: "http://localhost:5173",
     credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   }),
 );
+app.use(express.json());
+app.use(cookieParser());
 
 /**
  * -Routes Required
